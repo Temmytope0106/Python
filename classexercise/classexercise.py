159991bc4b7fd4e1afc97bda45d5883d30e7e346
Temmytope0106 <temitopejustine0@gmail.com>
2024-07-09 15:50:40 +0100

    Assignments

---
+++ classexercise/classexercise.py
@@ -0,0 +1,8 @@
+name = input("Enter your full name: ")
+age = int(input("Enter your age: "))
+
+if age >= 18:
+	print('You are eligible to make your decisions')
+	print('Thank you')
+else:
+	print('You are still my Child')
---
+++ classexercise/helloword.py
@@ -0,0 +1,5 @@
+name = input("Enter your name: ")
+password = input("Enter your password: ")
+password_length = len(password)
+print(password_length * "*")
+
